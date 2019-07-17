0624c20da752ce8a6d85f213a95c0daf8629e7b6
”Ozi <31310132+OziOcb@users.noreply.github.com>
2019-07-17 22:35:34 +0100

    Rafactore the TaskRemoveBtn.vue component

--- src/components/TaskRemoveBtn.vue
+++ src/components/TaskRemoveBtn.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="taskRemoveBtns">
+  <div>
     <v-btn
+      v-for="btn in btns"
+      :key="btn.id"
       @click.native="removeTask(task)"
-      class="hidden-sm-and-up ma-2"
-      fab
+      :class="`${btn.mq} ma-2`"
+      :fab="btn.type === 'fab'"
+      :rounded="btn.type === 'rounded'"
       dark
       color="red"
     >
-      <v-icon>mdi-delete</v-icon>
-    </v-btn>
-
-    <v-btn
-      @click.native="removeTask(task)"
-      class="hidden-xs-only ma-2"
-      color="red"
-      dark
-      rounded
-    >
-      Remove
-      <v-icon dark right>mdi-delete</v-icon>
+      {{ btn.text }}
+      <v-icon>{{ btn.icon }}</v-icon>
     </v-btn>
   </div>
 </template>
@@ -32,6 +25,26 @@ export default {
       type: Object,
       required: true
     }
+  },
+  data() {
+    return {
+      btns: [
+        {
+          id: 1,
+          mq: "hidden-sm-and-up",
+          type: "fab",
+          text: "",
+          icon: "mdi-delete"
+        },
+        {
+          id: 2,
+          mq: "hidden-xs-only",
+          type: "rounded",
+          text: "Remove",
+          icon: "mdi-delete"
+        }
+      ]
+    };
   },
   methods: mapActions(["removeTask"])
 };
