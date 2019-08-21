80cd40681bc5750d517f212bb6ba225082539084
”Ozi <31310132+OziOcb@users.noreply.github.com>
2019-08-21 22:30:03 +0100

    Add tooltips for the filter buttons

--- src/components/TaskFilterBtnsBar.vue
+++ src/components/TaskFilterBtnsBar.vue
@@ -1,16 +1,21 @@
 <template>
   <v-layout column>
-    <v-btn-toggle v-model="icon">
-      <v-btn
-        v-for="btn in btns"
-        :key="btn.id"
-        @click="$emit('changeFilterValue', btn.value)"
-        text
-        :value="`${btn.value}`"
-      >
-        <span class="hidden-sm-and-down">{{ btn.value }}</span>
-        <v-icon>{{ btn.icon }}</v-icon>
-      </v-btn>
+    <v-btn-toggle class="d-block" v-model="filter">
+      <v-tooltip top v-for="btn in btns" :key="btn.id">
+        <template v-slot:activator="{ on }">
+          <v-btn
+            style="width:33.333333%"
+            v-on="on"
+            @click="$emit('changeFilterValue', btn.value)"
+            text
+            :value="`${btn.value}`"
+          >
+            <span class="hidden-sm-and-down">{{ btn.value }}</span>
+            <v-icon>{{ btn.icon }}</v-icon></v-btn
+          >
+        </template>
+        <span>{{ btn.tooltip }}</span>
+      </v-tooltip>
     </v-btn-toggle>
   </v-layout>
 </template>
@@ -19,22 +24,25 @@
 export default {
   data() {
     return {
-      icon: "all",
+      filter: "all",
       btns: [
         {
           id: 1,
           value: "all",
-          icon: "mdi-file-document-box-outline"
+          icon: "mdi-file-document-box-outline",
+          tooltip: "show all tasks"
         },
         {
           id: 2,
           value: "completed",
-          icon: "mdi-file-document-box-check-outline"
+          icon: "mdi-file-document-box-check-outline",
+          tooltip: "show only completed tasks"
         },
         {
           id: 3,
           value: "uncompleted",
-          icon: "mdi-file-document-box-remove-outline"
+          icon: "mdi-file-document-box-remove-outline",
+          tooltip: "show only waiting tasks"
         }
       ]
     };
